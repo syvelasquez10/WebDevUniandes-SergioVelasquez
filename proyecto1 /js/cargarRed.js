4a3a88f5e18df0f641c71bc930073670737e00a9
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-02-14 17:10:26 -0500

    Funciona dar click en otro

--- proyecto1 /js/cargarRed.js
+++ proyecto1 /js/cargarRed.js
@@ -1,8 +1,13 @@
 "use strict";
+var network;
+var nodes;
+var edges;
+var nodos;
+var caminos;
 
-Red(document.getElementById("githubID").text);
+crearRed(document.getElementById("githubID").text);
 
-function Red(usuario) {
+function crearRed(usuario) {
     var valores;
 
    $.get("https://api.github.com/users/"+usuario+"/followers").then(function(data, status){
@@ -12,19 +17,18 @@ function Red(usuario) {
 
     function cargarRed() { 
         
-        var nodos =[{id:1,label:usuario}];
-        var caminos =[];
+        nodos =[{id:1,label:usuario}];
+        caminos =[];
         
         for(var i=0;i<valores.length;i++){
             nodos.push({id:i+2,label:valores[i].login});
             caminos.push({from:1,to:i+2});
         }
-        
         // create an array with nodes
-        var nodes = new vis.DataSet(nodos);
+        nodes = new vis.DataSet(nodos);
 
         // create an array with edges
-        var edges = new vis.DataSet(caminos);
+        edges = new vis.DataSet(caminos);
 
         // create a network
         var container = document.getElementById('mynetwork');
@@ -38,17 +42,57 @@ function Red(usuario) {
         var options = {};
 
         // initialize your network!
-        var network = new vis.Network(container, data, options);
+        network = new vis.Network(container, data, options);
+        listen();
+    }
+
+} 
+
+function nuevaRed(nodoInicio) {
+    var user = nodos[nodoInicio-1].label;
+    var valores;
+
+   $.get("https://api.github.com/users/"+user+"/followers").then(function(data, status){
+      valores=data;
+       cargarNuevaRed();
+    });
+
+    function cargarNuevaRed() { 
+        var contador= nodos.length+1;
+        for(var i=0;i<valores.length;i++){
+            nodos.push({id:i+contador,label:valores[i].login});
+            caminos.push({from:nodoInicio,to:i+contador});
+        }
+        // create an array with nodes
+        nodes= new vis.DataSet(nodos);
+
+        // create an array with edges
+        edges = new vis.DataSet(caminos);
+
+        // create a network
+        var container = document.getElementById('mynetwork');
+
+        // provide the data in the vis format
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+
+        var options = {};
+
+        // initialize your network!
+        network = new vis.Network(container, data, options);
+        listen();
     }
 
 } 
 
 
-
-setTimeout(function(){ 
-    
-    $('mynetwork').on("Click",function (params) {
-      console.log(params);
+function listen(){ 
+    network.on("click", function (params) {
+        console.log(params.nodes[0]);
+        if(!(params.nodes[0]===1)){
+            nuevaRed(params.nodes[0]);
+        }
     });
-    }
-,1300);
+}
